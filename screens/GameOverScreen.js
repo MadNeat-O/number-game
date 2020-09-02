a1ef6c48cc3cec2a641686dee150eae0c67a50f7
Brad Korman <brad.korman@remarque-creative.com>
2020-09-01 21:23:55 -0400

    added styling to GameOverScreen

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -4,15 +4,26 @@ import { View, Text, StyleSheet, Button, Image } from 'react-native';
 
 import BodyText from '../components/BodyText';
 import TitleText from '../components/TitleText';
+import Color from '../constants/colors';
 
 // create a component
 const GameOverScreen = (props) => {
     return(
         <View style={styles.screen}>
             <TitleText>Game Over!</TitleText>
-            <Image source={require('../assets/success.png')} style={styles.image} />
-            <BodyText>Your number was: {props.userNumber}</BodyText>
-            <BodyText>Number of Rounds: {props.numRounds}</BodyText>
+            <View style={styles.imageContainer}>
+                <Image 
+                    // source={require('../assets/success.png')} 
+                    source={{ uri: 'https://images.pexels.com/photos/314703/pexels-photo-314703.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260' }}
+                    style={styles.image}
+                />
+            </View>
+            <BodyText>
+                The computer took <Text style={styles.highlight}>{props.numRounds}</Text> rounds, 
+            </BodyText>
+            <BodyText>
+                to guess that your number was <Text style={styles.highlight}>{props.userNumber}</Text>
+                </BodyText>
             <Button title="New Game" onPress={props.onRestart} />
         </View>
     )
@@ -25,9 +36,22 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center'
     },
+    imageContainer: {
+        width: 300,
+        height: 300,
+        borderRadius: 150,
+        borderWidth: 3,
+        borderColor: 'black',
+        overflow: 'hidden',
+        marginVertical: 30
+    },
     image: {
-        width: '50%',
-        height: '25%'
+        width: '100%', 
+        height: '100%',
+    },
+    highlight: {
+        color: Color.primary,
+        fontFamily: 'open-sans-bold'
     }
 });
 
